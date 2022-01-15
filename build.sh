9bf14396e21855d0288d8667a361163c787aa846
Sean Marquez <capsulecorplab@gmail.com>
2022-01-14 23:16:19 -0700

    remove conditionals from build script

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
 # Install node modules
-if [ ! -r ./node_modules ]; then
-    docker run --rm --volume $PWD:/src -p 1313:1313 -w "/src" node:16.13.2-alpine3.15 npm ci
-fi
+docker run --rm --volume $PWD:/src -p 1313:1313 -w "/src" node:16.13.2-alpine3.15 npm ci
 
 # Run Hugo build
-if [ ! -r ./public ]; then
-    docker run --rm --volume $PWD:/src -p 1313:1313 -w "/src" peaceiris/hugo:v0.92.0-full --minify -v --destination public
-fi
+docker run --rm --volume $PWD:/src -p 1313:1313 -w "/src" peaceiris/hugo:v0.92.0-full --minify -v --destination public
